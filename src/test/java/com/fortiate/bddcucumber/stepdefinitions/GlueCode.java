45f05658eab0188f52cb8693d46c5cf8dca172bb
Amod D Kulkarni <amod.d.kulkarni@gmail.com>
2019-10-07 11:29:59 +0530

    added gecko driver

--- src/test/java/com/fortiate/bddcucumber/stepdefinitions/GlueCode.java
+++ src/test/java/com/fortiate/bddcucumber/stepdefinitions/GlueCode.java
@@ -1,11 +1,16 @@
 package com.fortiate.bddcucumber.stepdefinitions;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.cucumber.java8.En;
 
@@ -15,15 +20,10 @@ public class GlueCode implements En {
 
 	public GlueCode() {
 		
-		
-
 		Given("I am on a browser", () -> {
 		    // Write code here that turns the phrase above into concrete actions
 			System.setProperty("webdriver.chrome.driver", "chromedriver");
 			driver = new ChromeDriver();
-
-			
-			//throw new cucumber.api.PendingException();
 		});
 
 		When("I type http:\\/\\/localhost:{int} in the address bar", (Integer int1) -> {
@@ -34,29 +34,22 @@ public class GlueCode implements En {
 
 		Then("I should see the home login page", () -> {
 		    // Write code here that turns the phrase above into concrete actions
+			
+			try {
 			WebElement pageContent = driver.findElement(By.xpath("//*[text()='This site can’t be reached']"));
 			
-			if(pageContent.isDisplayed()) fail("The Application Was Not Accessible ! ");
-
+			//WebDriverWait wait = new WebDriverWait(driver, 60);
+			//wait.until(ExpectedConditions.visibilityOfElementLocated(By.id<locator>));
+			//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[text()='This site can’t be reached']")));
 			
+			if(pageContent.isDisplayed()) fail("The Application Was Not Accessible ! ");
+			
+			}
+			catch(NoSuchElementException e) {
+				assertTrue("Site was reachable",true);
+			}
 		    
 		});
-
-		Given("I have hungry snake", () -> {
-		    // Write code here that turns the phrase above into concrete actions
-		    throw new cucumber.api.PendingException();
-		});
-
-		When("When I feed my snake", () -> {
-		    // Write code here that turns the phrase above into concrete actions
-		    throw new cucumber.api.PendingException();
-		});
-
-		Then("I receive snek snek", () -> {
-		    // Write code here that turns the phrase above into concrete actions
-		    throw new cucumber.api.PendingException();
-		});
-
 		
 	}
 }
