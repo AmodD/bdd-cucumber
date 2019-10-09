0a6d406b32b2d6c8781ee6b2a1f687282db6d435
Amod D Kulkarni <amod.d.kulkarni@gmail.com>
2019-10-09 11:35:51 +0530

    setup changes

--- src/test/java/com/fortiate/bddcucumber/CucumberTests.java
+++ src/test/java/com/fortiate/bddcucumber/CucumberTests.java
@@ -3,8 +3,16 @@ package com.fortiate.bddcucumber;
 import org.junit.runner.RunWith;
 import io.cucumber.junit.*;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features", plugin = {"pretty", "html:target/cucumber"})
-public class CucumberTests {
+//https://sukesh15.gitbooks.io/cucumber-jvm-test-framework-/content/cucumber_-_more_details/scenario_tagging.html
 
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/resources/features", 
+		//glue = "src/test/java/com/fortiate/bddcucumeber/GlueCode.java" , 
+		plugin = {"pretty", "html:target/cucumber"}, 
+		tags = {"@WIP", "~@ON-HOLD"})
+public class CucumberTests {
+	
+	//https://www.toolsqa.com/cucumber/cucumber-options/
+	//https://testingneeds.wordpress.com/tag/cucumberoptions/
 }
