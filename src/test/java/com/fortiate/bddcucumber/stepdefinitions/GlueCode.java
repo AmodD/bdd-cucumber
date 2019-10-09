14c87114cc7855bc7fa90f2000c7f135281e5e77
Amod D Kulkarni <amod.d.kulkarni@gmail.com>
2019-10-09 10:43:51 +0530

    TV-86 tested in chrome firefox and safari

--- src/test/java/com/fortiate/bddcucumber/stepdefinitions/GlueCode.java
+++ src/test/java/com/fortiate/bddcucumber/stepdefinitions/GlueCode.java
@@ -1,6 +1,7 @@
 package com.fortiate.bddcucumber.stepdefinitions;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import org.openqa.selenium.By;
@@ -9,18 +10,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.cucumber.java8.En;
+import io.cucumber.java.en.Given;
 
 public class GlueCode implements En {
 
 	private WebDriver driver;
 
 	public GlueCode() {
-		
-		Given("I am on Chrome a browser", () -> {
+		 
+		Given("I am on a Chrome browser", () -> {
 			System.setProperty("webdriver.chrome.driver", "chromedriver");
 			driver = new ChromeDriver();
 		});
@@ -30,14 +33,38 @@ public class GlueCode implements En {
 		});
 
 		Then("I should see the product login page", () -> {
+			assertEquals("Fortiate Data Solutions",driver.getTitle());
+		});
+		
+		
+		Given("I am on a Firefox browser", () -> {
+			System.setProperty("webdriver.gecko.driver","geckodriver");
+			driver =new FirefoxDriver();
+		});
+		
+		Given("I am on a Safari browser", () -> {
+			driver =new SafariDriver();
+		});
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		Then("I should see the home login page", () -> {
 			try {
-			WebElement pageContent = driver.findElement(By.xpath("//*[text()='This site can’t be reached']"));
+			WebElement siteInAccessibleInChrome = driver.findElement(By.xpath("//*[text()='This site can’t be reached']"));
+			WebElement siteInAccessibleInFirefox = driver.findElement(By.xpath("//*[text()='Unable to connect']"));
 			
 			//WebDriverWait wait = new WebDriverWait(driver, 60);
 			//wait.until(ExpectedConditions.visibilityOfElementLocated(By.id<locator>));
 			//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[text()='This site can’t be reached']")));
 			
-			if(pageContent.isDisplayed()) fail("The Application Was Not Accessible ! ");
+			if(siteInAccessibleInChrome.isDisplayed()) fail("The Application Was Not Accessible In Chrome ! ");
+			if(siteInAccessibleInFirefox.isDisplayed()) fail("The Application Was Not Accessible In Firefox ! ");
 			
 			}
 			catch(NoSuchElementException e) {
@@ -46,10 +73,6 @@ public class GlueCode implements En {
 		    
 		});
 		
-		Given("I am on a Firefox browser", () -> {
-			System.setProperty("webdriver.gecko.driver","geckodriver");
-			driver =new FirefoxDriver();
-		});
 		
 	}
 }
--- src/test/resources/features/tv-in-various-browsers.feature
+++ src/test/resources/features/tv-in-various-browsers.feature
@@ -1,5 +1,6 @@
+@WIP
 Feature: TV in various browsers
-
+  
   compatibility check to see if the TV application opens up in different browsers
 
   @TV-8 @OPEN
@@ -7,25 +8,21 @@ Feature: TV in various browsers
     Given I am on a Chrome browser
     When I type http://localhost:48000 in the address bar
     Then I should see the product login page
-    And The title of the page should be "Fortiate Data Solutions"
 
   @TV-8 @OPEN
   Scenario: Viewing in a Firefox browser
     Given I am on a Firefox browser
     When I type http://localhost:48000 in the address bar
     Then I should see the product login page
-    And The title of the page should be "Fortiate Data Solutions"
 
-  @TV-8 @OPEN
+  @TV-8 @OPEN @ON-HOLD
   Scenario: Viewing in a IE browser
     Given I am on a IE browser
     When I type http://localhost:48000 in the address bar
     Then I should see the product login page
-    And The title of the page should be "Fortiate Data Solutions"
 
   @TV-8 @OPEN
   Scenario: Viewing in a Safari browser
     Given I am on a Safari browser
     When I type http://localhost:48000 in the address bar
     Then I should see the product login page
-    And The title of the page should be "Fortiate Data Solutions"
