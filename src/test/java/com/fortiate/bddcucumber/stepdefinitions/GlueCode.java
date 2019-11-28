73c6a1c5b90c93a95260b977bf1a3cd85f7c0cdf
AmrutaDravid <30amruta@gmail.com>
2019-11-28 16:39:19 +0530

    #TV-219 modified glue code

--- src/test/java/com/fortiate/bddcucumber/stepdefinitions/GlueCode.java
+++ src/test/java/com/fortiate/bddcucumber/stepdefinitions/GlueCode.java
@@ -1,148 +1,89 @@
 package com.fortiate.bddcucumber.stepdefinitions;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+
 
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.NoSuchElementException;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+
 
 import io.cucumber.java8.En;
-import junit.framework.Assert;
-import io.cucumber.java.en.Given;
+
 
 public class GlueCode implements En {
 
 	private WebDriver driver;
 
 	public GlueCode() {
-		 
-		/*Given("I am on a Chrome browser", () -> {
-			System.setProperty("webdriver.chrome.driver", "chromedriver");
-			driver = new ChromeDriver();
-		});
 
-		When("I type http:\\/\\/localhost:{int} in the address bar", (Integer int1) -> {
-		    driver.navigate().to("http://localhost:48000");
-		});
-
-		Then("I should see the product login page", () -> {
-			assertEquals("Fortiate Data Solutions",driver.getTitle());
-		});*/
-		
-		
 		Given("I have entered http://localhost:48000", () -> {
-			
-			
-			System.setProperty("webdriver.gecko.driver", "/home/amit/FORTIATE/BUILD/WORKSPACES/JAVA/bdd-cucumber/geckodriver-v0.26.0-linux64/geckodriver");
-			driver =new FirefoxDriver();
-			
-			
-		    
-			//throw new cucumber.api.PendingException();
-			
-			});
+			System.setProperty("webdriver.gecko.driver", "/home/amit/FORTIATE/BUILD/WORKSPACES/JAVA/bdd-cucumber/geckodriver");
+			 driver = new FirefoxDriver();
+			//driver.navigate().to("http://localhost:48000");
+		
+		});
+		
+		try {
 		And("I see the login page",() -> {
 			driver.navigate().to("http://localhost:48000");
 		});
-		/*Given("I am on a Safari browser", () -> {
-			driver =new SafariDriver();
-		});*/
+		}
+		catch(Exception e)
+		{
+			
+		}
+	
+		When("I enter valid user ID and Password", () -> {
+	
+			driver.findElement(By.name("email")).sendKeys("amit@fortiate.com");
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			driver.findElement(By.name("password")).sendKeys("abcdef12");
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		});	
+
+		Then("I should see the dashboard page", () -> {
+			driver.findElement(By.name("button")).click();
+			
+			});
+
+
+	   When("I enter invalid user ID and valid Password", () -> {
+			driver.findElement(By.name("email")).sendKeys("gfdgfdgdfgdfgfddfg");
+			driver.findElement(By.name("password")).sendKeys("abcdef12");
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        });
+	   
+	   When("I enter valid user ID and invalid Password", () -> {
+			driver.findElement(By.name("email")).sendKeys("amit@fortiate.com");
+			driver.findElement(By.name("password")).sendKeys("hghghghjghjgjghg");
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		
+       });
+         
 
-/*Given("I have entered http:\\/\\/localhost:{int}", (Integer int1) -> {
-	System.setProperty("webdriver.chrome.driver", "chromedriver");
-	driver = new ChromeDriver();
-    driver.navigate().to("http://localhost:48000");
-
-});*/
-
-/*Given("I see the login page", () -> {
-	if (!(driver.findElement(By.id("login_page"))).isDisplayed()) fail("Login Page Not Displayed");
-});*/
-
-When("I enter valid user ID and Password", () -> 
-{
-	
-	driver.findElement(By.name("email")).sendKeys("amit@fortiate.com");
-	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    driver.findElement(By.name("password")).sendKeys("abcdef12");
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    //throw new cucumber.api.PendingException();
-	
-	});
-
-Then("I should see the dashboard page", () -> {
-    
-	
-	driver.findElement(By.name("button")).click();
-    //throw new cucumber.api.PendingException();
+		Then("I should see an error message", () -> {
+		
+			
+			 
+	         WebElement emailmsg = driver.findElement(By.id("emailerror"));
+	         emailmsg.getText();
+			
+			WebElement pwdmsg = driver.findElement(By.id("passworderror"));
+			pwdmsg.getText();
+			
+			//driver.findElement(By.name("button")).click();
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+					
 });
-
-When("I enter invalid user ID and valid Password", () -> {
-	driver.findElement(By.name("email")).sendKeys("amit123@fortiate.com");
-	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    driver.findElement(By.name("password")).sendKeys("abcdef12");
-    //driver.findElement(By.name("button")).click();
-});
-
-Then("I should see an error message ", () -> {
-	 
-	 WebElement actualmsg = driver.findElement(By.id("emailerror"));
-	 driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	String errormsg="invalid login id";
 	
-	if(errormsg.equalsIgnoreCase((actualmsg).getText()))
-	{
-		System.out.println("test case passed");
-	}
-	else
-	{
-		System.out.println("test case failed");
-	}
-	
-
-   
-});
-
-When("I enter Valid user ID and Invalid Password", () -> {
-	driver.findElement(By.name("email")).sendKeys("amit@fortiate.com");
-	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    driver.findElement(By.name("password")).sendKeys("abcdef12345");
-    //driver.findElement(By.name("button")).click();
-    //throw new cucumber.api.PendingException();
-});
-
-Then("I should see an error message", () -> {
-	WebElement actualmsg = driver.findElement(By.id("passworderror"));
-	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	String errormsg="invalid password";
-	
-	if(errormsg.equalsIgnoreCase((actualmsg).getText()))
-	{
-		System.out.println("test case passed");
-	}
-	else
-	{
-		System.out.println("test case failed");
+		
 	}
 
-    driver.close();
-});
-	
-	
-}
-	
 }
 	
 
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -8,16 +8,18 @@ Feature: Login
     When I enter valid user ID and Password
     Then I should see the dashboard page
 
-  @TV-4 @OPEN
-  Scenario: Login using Invalid ID and valid password
+ @TV-4 @OPEN
+  Scenario: Login using ID and Password
     Given I have entered http://localhost:48000 
     And I see the login page
     When I enter invalid user ID and valid Password
     Then I should see an error message
 
-  @TV-4 @OPEN
-  Scenario: Login using Valid ID and Invalid Password
+ @TV-4 @OPEN
+  Scenario: Login using ID and Password
     Given I have entered http://localhost:48000 
     And I see the login page
-    When I enter Valid user ID and Invalid Password
+    When I enter valid user ID and invalid Password
     Then I should see an error message
+
+ 
